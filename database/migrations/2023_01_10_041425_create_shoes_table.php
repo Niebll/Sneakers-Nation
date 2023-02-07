eb31a1fc8f294b46288ba2005a1db977ac28c00c
Nabil <nabilrajankanz@gmail.comm>
2023-02-07 11:55:01 +0700

    first commit

---
+++ database/migrations/2023_01_10_041425_create_shoes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shoes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->integer('price');
+            $table->foreignID('merk_id');
+            $table->foreignID('type_id');
+            $table->enum('gender', ['M', 'F']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shoes');
+    }
+};
